c338054871b12ac57e530bbaef7ac7ba9ccbfbcc
Mike McGuire <mikemcguire.dev@gmail.com>
2015-06-25 09:30:29 -0600

    Seting up router and basic styles

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-document.write('Hello Frontend Masters!')
+import Router from "./router"
+import styles from "./styles/styles.css"
+window.app = {
+	init() {
+		this.router = new Router()
+		this.router.history.start()
+	}
+}
+
+window.app.init()
